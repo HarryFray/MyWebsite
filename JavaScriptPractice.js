62cbe54cea164829b4847296a002d40511f07392
Nicholas H Fray <harry.fray7@gmail.com>
2017-11-28 17:21:51 -0800

    First time with JavaScript!

---
+++ JavaScriptPractice.js
@@ -0,0 +1,11 @@
+var dogs = 2
+switch (dogs){
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  console.log('only ' + dogs +'!');break;
+  case 6:
+  console.log('half o-12 pack o-puppers');
+}
