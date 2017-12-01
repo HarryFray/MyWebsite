06431fe927e615172b2b1d7716a95fd9e7473147
Nicholas H Fray <harry.fray7@gmail.com>
2017-12-01 09:43:43 -0800

    Version 5
    Now toggles completed and displays status of entire list. no visible webpage

--- Todo.js
+++ Todo.js
@@ -1,12 +1,19 @@
-// WatchAndCode Todo Exercise
-
-// To do app for chrome
-var todos = {
+var todoList = {
   todos: [],
-  display: function() {
-    console.log('My Todos', this.todos);
-  },
 
+  // Display Todo text and completion status
+  display: function() {
+    if (this.todos.length === 0) {
+      console.log('your todos list is empty');
+    } else {
+      console.log('My To Dos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].completed ? '(X)' : '( )', this.todos[i].item_text);
+      }
+
+    }
+  },
+  // Add an Object
   addTd: function(item_text) { // adds new object to todos
     this.todos.push({
       item_text: item_text,
@@ -15,20 +22,59 @@ var todos = {
     this.display();
   },
 
-  // changes an object in todos
-  changeTd: function(index, newText) {
+  // Change an Object
+  changeTd: function(index, newText) { // changes an object in todos
     this.todos[index].item_text = newText;
     this.display();
   },
 
-  // deletes a todo in todos
+  // Detele an Object
   deleteTd: function(index) {
     this.todos.splice(index, 1);
     this.display();
   },
-  toggleCompleted: function(index){
-  var todo = this.todos[index];
-  todo.completed = !todo.completed;
-  this.display();
+  // True False toggle for an object
+  toggle: function(index) { // changes booien in each todo object
+    var todo = this.todos[index];
+    todo.completed = !todo.completed;
   },
+
+  // True/ False toggle for all objects
+  toggleAll: function() {
+    totalTrue = 0
+    for (var i = 0; i < this.todos.length; i++) {
+      if (this.todos[i].completed === true)
+        totalTrue++;
+    }
+    // CASE 1: if everything is True make everything False
+    if (totalTrue === this.todos.length) {
+      for (var i = 0; i < this.todos.length; i++) {
+        this.toggle(i)};
+
+
+    // CASE 2: Make everything True
+    } else {
+      for (var i = 0; i < this.todos.length; i++) { // changes all to true
+        this.todos[i].completed = true;
+      }
+    }
+    this.display()
+  },
+
+
+
 };
+
+/*
+todoList.addTd('item 1')
+todoList.addTd('item 2')
+todoList.addTd('item 3')
+todoList.addTd('item 4')
+todoList.addTd('item 5')
+
+todoList.toggle(0)
+todoList.toggle(1)
+todoList.toggle(2)
+todoList.toggle(3)
+todoList.toggle(4)
+*/
