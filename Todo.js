85b7a92842f482ba54f52a1e2f69db151ac2b361
Nicholas H Fray <harry.fray7@gmail.com>
2017-12-02 09:53:59 -0800

    Now runs without a console

--- Todo.js
+++ Todo.js
@@ -19,19 +19,17 @@ var todoList = {
       item_text: item_text,
       completed: false,
     });
-    this.display();
   },
 
   // Change an Object
   changeTd: function(index, newText) { // changes an object in todos
     this.todos[index].item_text = newText;
-    this.display();
   },
 
   // Detele an Object
   deleteTd: function(index) {
     this.todos.splice(index, 1);
-    this.display();
+
   },
   // True False toggle for an object
   toggle: function(index) { // changes booien in each todo object
@@ -49,32 +47,65 @@ var todoList = {
     // CASE 1: if everything is True make everything False
     if (totalTrue === this.todos.length) {
       for (var i = 0; i < this.todos.length; i++) {
-        this.toggle(i)};
+        this.toggle(i)
+      };
 
 
-    // CASE 2: Make everything True
+      // CASE 2: Make everything True
     } else {
       for (var i = 0; i < this.todos.length; i++) { // changes all to true
         this.todos[i].completed = true;
       }
     }
-    this.display()
   },
-
-
-
 };
 
-/*
-todoList.addTd('item 1')
-todoList.addTd('item 2')
-todoList.addTd('item 3')
-todoList.addTd('item 4')
-todoList.addTd('item 5')
 
-todoList.toggle(0)
-todoList.toggle(1)
-todoList.toggle(2)
-todoList.toggle(3)
-todoList.toggle(4)
-*/
+
+// Handdles user interaction or user interface UI
+var handlers = {
+  addTd: function() {
+    var addTodoTextInput = document.getElementById('addTodoTextInput');
+    todoList.addTd(addTodoTextInput.value) // value just used when calling an imput?
+    addTodoTextInput.value = ""
+    view.displayTd()
+  },
+  changeTd: function() {
+    var changeTdIndexInput = document.getElementById('changeTdIndexInput');
+    var changeTdTextInput = document.getElementById('changeTdTextInput');
+    todoList.changeTd(changeTdIndexInput.valueAsNumber, changeTdTextInput.value)
+    changeTdIndexInput.value = ""
+    changeTdTextInput.value = ""
+    view.displayTd()
+  },
+  deleteTd: function() {
+    var deleteTdInput = document.getElementById('deleteTdInput');
+    todoList.deleteTd(deleteTdInput.valueAsNumber)
+    deleteTdInput.value = ""
+    view.displayTd()
+  },
+  toggleCompleted: function() {
+    var toggleCompletedInput = document.getElementById('toggleCompletedInput');
+    todoList.toggle(toggleCompletedInput.valueAsNumber)
+    toggleCompletedInput.value = ""
+    view.displayTd()
+  },
+  toggleAll: function() {
+    todoList.toggleAll()
+    view.displayTd()
+  },
+};
+
+// responsable for what users see
+var view = {
+  displayTd: function() {
+    var todoUl = document.querySelector('ul') // new method finds element location
+    todoUl.innerHTML = '' // points inside specified element
+
+    for (var i = 0; i < todoList.todos.length; i++) {
+      var todoLi = document.createElement('li') // creates a new element in html
+      todoLi.textContent = todoList.todos[i].completed ? '(X) ' + todoList.todos[i].item_text : '( ) ' + todoList.todos[i].item_text; // new method adds content to tag
+      todoUl.appendChild(todoLi) // appends new child element within specified parent element
+    }
+  }
+};
