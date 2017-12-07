fed92dfe8bdc33c39ea5e692677ce05b49fac31a
Nicholas H Fray <harry.fray7@gmail.com>
2017-12-07 12:48:04 -0800

    hackReactorProblemSet
    Module 1

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -1,114 +1,210 @@
-var dogs = 2
-switch (dogs){
-  case 1:
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-  console.log('only ' + dogs +'!');break;
-  case 6:
-  console.log('half o-12 pack o-puppers');
+
+
+var person = {
+  name : 'Sam',
+  age : 25,
+  hasPets : true
 }
 
-var x = 1;
+function getAllKeys(obj){
+var  array = []
+for( i in obj){
+  array.push(i)
+}
+return array
+}
+//////////////////////////////////////////////////////////////////////
+var vehicle = [['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]]
 
-while ( x <= 20) {
-    if (x % 3 == 0){
-        console.log('Julia')
-    }else if(x % 5 == 0){
-        console.log('Nick')
-    }else if(x % 3 == 0 && x % 5 == 0){
-        console.log('JuliaNick')
-    }else{
-        console.log(x)
-    }
-    var x = x + 1
-  }
-
-
-
-
-
-  function makeLine(length) {
-      var line = "";
-      for (var j = 1; j <= length; j++) {
-          line += "* ";
-      }
-      return line + "\n";
-  }
-
-  // your code goes here.  Make sure you call makeLine() in your own code.
-  function buildTriangle(ww){
-      for (var i = 1; i < ww; i++){
-         makeLine(i);
-      }
-  }
-
-  // test your code by uncommenting the following line
-  console.log(buildTriangle(10));
-
-
-
-
-var sports = function(balls){
-  var ball = balls**2
-  return ball
+function fromListToObject(array) {
+  var newObj = {}
+  array.forEach(function(current){
+    newObj[current[0]] = current[1]
+})
+return newObj
 }
 
-sports(10)
+/////////////////////////////////
 
-var numbers = [
-    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
-    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
-    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
-    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
-    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
-    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
-    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
-    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
-    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
-    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
-];
-
-// your code goes here
-for(c=0; c < numbers.length(); ++c){
-    console.log(numbers[c])
+var person = {
+  name : 'Krysten',
+  age : 33,
+  hasPets : false
 }
 
-var savingsAccount = {
-    balance: 1000,
-    interestRatePercent: 1,
-    deposit: function addMoney(amount) {
-        if (amount > 0) {
-            savingsAccount.balance += amount;
-        }
-    },
-    withdraw: function removeMoney(amount) {
-        var verifyBalance = savingsAccount.balance - amount;
-        if (amount > 0 && verifyBalance >= 0) {
-            savingsAccount.balance -= amount;
-        }
-    },
-    printAccountSummary: function printAccountSummary(){
-        return 'your balance is currently ' + savingsAccount.balance + ' and your interest rate is '+ savingsAccount.interestRatePercent +'%'
+function listAllValues(obj) {
+var  array = []
+for( i in obj){
+  array.push(obj.i)
+}
+return array
+}
 
-    }
+/////////////////////////////////////////
+
+var employees = [
+    [  ['firstName', 'Joe'], ['lastName', 'Blow'], ['age', 42], ['role', 'clerk']
+    ],
+    [  ['firstName', 'Mary'], ['lastName', 'Jenkins'], ['age', 36], ['role', 'manager']
+    ]
+]
+
+function transformEmployeeData(masterArray) {
+    var newArray = [];
+    masterArray.forEach(function(object){
+      var newObject = {};
+      object.forEach(function(current){
+        newObject[current[0]] = current[1];
+    })
+      newArray.push(newObject);
+  })
+    return newArray
 };
 
-console.log(savingsAccount.printAccountSummary());
+/////////////////////////////////////////////////////
+
+var dog = {
+  species: 'canine',
+  name: 'Bowser',
+  weight: 45
+}
 
 
+function convertObjectToList(obj) {
+  newList = []
+  for(i in obj){
+  newList.push([i,obj[i]])
+}
+return newList
 
-var facebookProfile ={
-    name:'nick',
-    friends:14,
-    messages:['pumped for weekedn','shit is wack out here','honeys are fly today!'],
-    postMessage: function(post){
-        return facebookProfile.messages.push(post)},
-    deleteMessage: function(index){
-        return facebookProfile.message.splice(index,1)},
-    addFriend: function(){
-        return ++facebookProfile.friends},
-    removeFriend: function(){
-        --facebookProfile.friends}
+}
+
+
+////////////////////////////////////////////////////
+
+var customerData = {
+  'Joe': {
+    visits: 0
+  },
+  'Carol': {
+    visits: 2
+  },
+  'Howard': {
+    visits: 3
+  },
+  'Carrie': {
+    visits: 4
+  }
 };
+
+function greetCustomer(firstName) {
+ var visits = customerData[firstName].visits
+ console.log(visits)
+  switch(visits){
+case 0:
+    greeting ='Welcome! Is this your first time?'
+    break;
+case 1:
+    greeting ='Welcome back, ' + firstName + '! We\'re glad you liked us the first time!'
+    break;
+default:
+    greeting = 'Welcome back, ' + firstName + '! So glad to see you again!'
+    break;
+}
+  return greeting;
+}
+
+/////////////////////////////////
+
+var str = 'hey my name name my name is nick'
+
+function countWords(str) {
+  var wordCount = {};
+  var words = str.split(' ');
+  for (index in words){
+    wordCount[words[index]] = 1
+  }
+return wordCount
+}
+
+countWords(str)
+
+
+////////////////////////////////////////
+
+var oobj1 = {
+  a: 1,
+  b: 2
+};
+var oobj2 = {
+  b: 4,
+  c: 3
+};
+
+
+function extend(obj1, obj2) {
+  for(key in obj2){
+    if (!(key in obj1)){
+      obj1[key] = obj2[key]
+    }
+  }
+  return obj1
+}
+
+extend(oobj1,oobj2)
+
+///////////////////////////////////
+var oobj = {
+x: 10,
+y: 1,
+z: 2
+};
+
+
+function removeNumbersLargerThan(num, obj) {
+  for ( key in obj){
+    console.log(obj[key])
+    if (obj[key] > num){
+      delete obj[key]
+    }
+  }
+  return obj
+}
+
+removeNumbersLargerThan(5,oobj)
+
+
+/////////////////////////////////////
+
+
+var oobj = {
+  name: 'Montana',
+  age: 20,
+  location: 'Texas'
+};
+
+function removeStringValuesLongerThan(num, obj) {
+  for (key in obj){
+    if( obj[key].length > num){
+      delete obj[key]
+    }
+  }
+  return obj
+}
+
+removeStringValuesLongerThan(10,oobj)
+
+
+//////////////////////////////////
+
+var input = [1, 2];
+addToFrontOfNew(input, 3);
+
+
+function addToFrontOfNew(arr, element) {
+  debugger;
+  var newArray = arr
+  newArray.splice(0,0,element);
+  return newArray
+  console.log(arr)
+}
