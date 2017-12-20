a425f3fcee5f0654a6dae39599100b470a112456
Nicholas H Fray <harry.fray7@gmail.com>
2017-12-20 10:01:17 -0800

    Add files via upload

--- JavaScriptPractice.js
+++ JavaScriptPractice.js
@@ -726,3 +726,278 @@ function convertScoreToGrade(score) {
   }
 }
 ////////////////////////////////////
+/*
+Notes:
+* (100 - 90) --> 'A'
+* (89  - 80) --> 'B'
+* (79  - 70) --> 'C'
+* (69  - 60) --> 'D'
+* (60  -  0) --> 'F'
+* If the given score is greater than 100 or less than 0, it should return 'INVALID SCORE'.
+* If the score is between the 0 and 2 (inclusive) of a given range, return the letter with a '-'
+* If the score is be the 8 and 9 (inclusive) of a given range, return the letter with a '+'
+* There are is no F+ and there is no F-.
+
+var output = convertScoreToGradeWithPlusAndMinus(91);
+console.log(output); // --> 'A-'
+*/
+
+function convertScoreToGradeWithPlusAndMinus(score) {
+  var fstDig = score.toString()[0]
+  var secDig = score.toString()[1]
+  var letter = ''
+
+  if (score > 100 || score < 0){
+    return 'INVALID SCORE'
+  }
+
+  if (score === 100 || fstDig === '9'){
+    letter = 'A'
+  }else if( fstDig === '8'){
+    letter = 'B'
+  }else if( fstDig === '7'){
+    letter = 'C'
+  }else if( fstDig === '6'){
+    letter = 'D'
+  }else{
+
+  }
+
+
+  if (secDig >= '8'){
+    return letter + '+'
+  }else if (secDig > '2'){
+    return letter
+  }else{
+    return letter + '-'
+  }
+
+}
+
+
+///////////////////////////
+
+
+function computeFactorialOfN(n) {
+  if (n === 0){
+    return 1
+  }else{
+    return n * computeFactorialOfN(n-1)
+  }
+}
+
+//////////////////////////////////
+
+computeCompoundInterest(1500, .043, 4, 6);
+ // --> 438.836
+
+function computeCompoundInterest(principal, interestRate, compoundingFrequency, timeInYears) {
+  return  principal * ((1 + (interestRate / compoundingFrequency)) ** (timeInYears * compoundingFrequency)) - principal
+}
+
+
+////////////////////////
+/*
+Notes:
+* Do NOT use the actual built-in modulo (aka "remainder") operator (%) in your implementation.
+* 0 % ANYNUMBER = 0.
+* ANYNUMBER % 0 = NaN.
+* If either operand is NaN, then the result is NaN.
+* Modulo always returns the sign of the first number.
+*/
+modulo(25, 4);
+// --> 1
+
+function modulo(num1, num2) {
+  var floorVal = Math.floor(num1 / num2)
+  return num1 - (floorVal * num2)
+}
+
+///////////////////////////////////////
+
+function isOddWithoutModulo(num) {
+  var float = num / 2
+  if (float)
+
+}
+
+
+///////////////////////////////////////////
+
+function multiplyBetween(num1, num2) {
+  if ( num2 < num1 || num1 === num2){
+    return 0
+  }else{
+    if ( num2 === num1 + 1){
+      return num1
+    }else{
+      return num1 * multiplyBetween(num1 + 1, num2)
+    }
+  }
+}
+
+
+
+//////////////////
+
+function computeSumBetween(num1, num2) {
+  if (num1 === num2 || num2 < num1){
+    return 0
+  }else{
+    if (num1 + 1 === num2){
+      return num1
+    }else{
+      return num1 + computeSumBetween(num1 + 1, num2)
+    }
+  }
+}
+
+
+//////////////////////////
+
+
+countWords('ask a bunch get a bunch');
+ // --> {ask: 1, a: 2, bunch: 2, get: 1}
+
+
+function countWords(str) {
+  var wordCount = {}
+  if (str === ''){
+    return wordCount
+  }else{
+    var words = str.split(' ')
+    words.forEach(function(word){
+      if (word in wordCount){
+        ++wordCount[word]
+      }else{
+        wordCount[word] = 1
+      }
+    })
+  return wordCount
+}
+}
+
+
+/////////////////////
+
+
+var pets = {
+  dog: 'bark!',
+  cat: 'meow!'
+}
+
+
+
+
+////////////////////////////////
+
+
+var x = 7;
+var y = 8;
+var z = 9;
+
+x = y + z;
+z = x;
+x = y;
+z = x + y;
+y = z - x;
+z = y;
+x = y * z;
+z = y / z;
+x = (z - x) / (y + z);
+
+//////////////////////////////////
+
+
+function multiplyByTwo(n) {
+  return (n * 2) + 1; // an incorrect implementation
+}
+
+//var output = multiplyByTwo(2); // returns 5
+assertEqual(multiplyByTwo(2), 4, 'it doubles 2 to 4');
+
+// FAILED [it doubles 2 to 4] Expected "4", but got "5"
+function assertEqual(actual, expected, testName) {
+  if( actual === expected){
+    console.log('passed')
+  }else{
+    console.log('FAILED [' + testName + '] Expected "' + expected + '" but got "' + actual + '"')
+  }
+}
+
+/////////////////////
+
+/*
+var expected = [1, 1, 2, 3, 5, 8, 13];
+var actual = generateFirstNFibonaccis(7); //just an example
+assertArraysEqual(actual, expected, 'generates first 7 Fibonacci numbers');
+// console output:
+// FAILED [generates first 7 Fibonacci numbers] Expected "1, 1, 2, 3, 5, 8, 13", but got "2, 3, 5, 8, 13, 21, 34"
+
+*/
+
+function assertArraysEqual(actual, expected, testName) {
+  if( compareArrays(actual, expected) ){
+    console.log ('passed')
+  }else{
+    console.log( 'FAILED [' + testName + '] Expected "' + expected +'", but got "' + actual + '"')
+  }
+}
+
+function compareArrays(actual, expected){
+  for(i = 0; i < expected.length; i++){
+    if (actual[i] !== expected[i]){
+      return false
+    }
+  }
+  return true
+}
+
+
+//////////////////
+
+/*
+
+var person = {
+  firstName: 'Joe',
+  lastName: 'Blow'
+};
+
+person.firstName = 'jack'
+
+var expected = {
+  firstName: 'Jack',
+  lastName: 'Blow'
+};
+
+assertObjectsEqual(person, expected, 'updates person's existing first name field');
+// console output:
+// passed
+
+FAILURE CASE
+
+var person = {
+  firstName: 'Joe',
+  lastName: 'Blow'
+};
+person = updateObject(person, 'age', 35);
+
+var expected = {
+  firstName: 'Joe',
+  lastName: 'Blow',
+  age: 35
+};
+assertObjectsEqual(person, expected, 'adds person's non-existing age field');
+// console output:
+// FAILED [adds person's non-existing age field] Expected {"firstName":"Joe", "lastName":"Blow", age:35}, but got {"firstName":"Joe", "lastName":"Blow"}
+
+*/
+function assertObjectsEqual(actual, expected, testName) {
+  var actualJson = JSON.stringify(actual)
+  var expectedJson = JSON.stringify(expected)
+  if ( actualJson === expectedJson ){
+      console.log('passed')
+      }else{
+        console.log( 'FAILED [' + testName + '] Expected ' + JSON.stringify(expected) +', but got ' + JSON.stringify(actual) )
+      }
+  }
